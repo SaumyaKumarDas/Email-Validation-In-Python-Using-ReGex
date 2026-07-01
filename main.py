b5974df699141bf052b54c6696ab9ab6de2b25bc
SaumyaKumarDas <dassaumya13@gmail.com>
2026-07-01 22:28:11 +0545

    Update

---
+++ main.py
@@ -0,0 +1,10 @@
+import regex as re
+
+
+
+email_condition = "^[a-z]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$"
+user_email = input("Enter your email address:")
+if re.search(email_condition, user_email):
+    print("Email address is valid")
+else:
+    print("Email address is not valid")
